cfcffb65881179e25effb2e034c9109ad51f783d
guy kaplan <guykap4@gmail.com>
2021-06-23 12:35:51 +0300

    Auto-generated commit [ci skip]

--- src/app/models/move-model.ts
+++
@@ -1,7 +0,0 @@
-export interface Move {
-    senderId: string,
-    amount: number,
-    receiverId: string,
-    timestamp: number,
-    receiverName: string,
-}
--- src/app/models/user-model.ts
+++
@@ -1,9 +0,0 @@
-import { Move } from "./move-model";
-
-export interface User {
-    id?: string,
-    name: string,
-    coins: number,
-    moves: Move[],
-    img: String,
-}
